1347f520b2dd743a9ef03c326ecce3a0ca2823d7
Tevz <tevz.kristan2007@gmail.com>
2023-12-31 15:43:50 +0100

    update

--- app.js
+++ app.js
@@ -18,3 +18,27 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 });
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    // Add animation to each nav link
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.7
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
